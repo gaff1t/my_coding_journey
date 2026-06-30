a9f3aba0bf27cb1846688b251918ae614b1b734b
gleb-cybersec <petsevichg@gmail.com>
2026-06-30 14:35:26 +0300

    code fix

--- month_01_foundations/week_01/lesson_01_calculator/main.py
+++ month_01_foundations/week_01/lesson_01_calculator/main.py
@@ -1,15 +1,15 @@
 # Это мой первый скрипт. Он считает, сколько дней я прожил.
 
 age = 25
-days_in_year = 365
-hours_in_a_day = 24
-minutes_in_an_hour = 60
-weeks_a_year = 52
+days_per_year = 365
+hours_per_day = 24
+minutes_per_hour = 60
+weeks_per_year = 52
 
-days_lived = age * days_in_year
-hours_lived = age * days_in_year * hours_in_a_day
-minutes_lived = age * days_in_year * hours_in_a_day * minutes_in_an_hour
-weeks_lived = age * weeks_a_year
+days_lived = age * days_per_year
+hours_lived = days_lived * hours_per_day
+minutes_lived = hours_lived * minutes_per_hour
+weeks_lived = age * weeks_per_year
 
 print("Прожито недель: ", weeks_lived)
 print("Прожито дней: ", days_lived)
