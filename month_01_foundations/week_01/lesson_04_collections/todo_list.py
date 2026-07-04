483082edb8759454ccd3701bec06f753628036a4
gleb-cybersec <petsevichg@gmail.com>
2026-07-04 18:45:34 +0300

    the add and remove command has been improved

--- month_01_foundations/week_01/lesson_04_collections/todo_list.py
+++ month_01_foundations/week_01/lesson_04_collections/todo_list.py
@@ -9,11 +9,25 @@ while True:
         print("Пока.")
         break # выходим из цикла
     elif command == "добавить":
-        tasks.append(input("Какую задачу хотите добавить? ")) # добавляем задачу ввенденую пользователем
+        added = input("Какую задачу хотите добавить? ") # запрашиваем задачу, которую необходимо добавит
+        tasks.append(added) # добавляем задачу
+        print(f"Задача '{added}' добавлена!")
     elif command == "удалить":
-        number = int(input("Введите номер задачи для удаления: ")) # запрашиваем номер задачи
-        removed = tasks.pop(number - 1)
-        print(f"Задача '{removed}' удалена!")
+        if len(tasks) == 0: # проверяем, есть ли данные в списке
+            print("Список пуст.")
+        else:
+            # показываем список
+            for index, item in enumerate(tasks):
+                print(f"{index + 1}. {item}")
+            
+            print()
+            number = int(input("Введите номер задачи для удаления: ")) # запрашиваем номер задачи
+            
+            if number < 1 or number > len(tasks): # проверяем наличе номера задачи
+                print("Задачи с таким номером нет.")
+            else:
+                removed = tasks.pop(number - 1) # удаляем значение и присваеваем его переменной
+                print(f"Задача '{removed}' удалена!") # выводим удаленное значение
     elif command == "показать":
         if len(tasks) == 0:
             print("Список пуст.")
